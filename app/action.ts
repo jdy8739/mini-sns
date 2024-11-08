6b01b43fa171821efd379c67a2fff8807c63c5b1
jdy8739 <jdy8739@naver.com>
2024-11-08 22:28:00 +0900

    feat: certify form elements

--- app/action.ts
+++ app/action.ts
@@ -1,19 +1,33 @@
+/* eslint-disable no-param-reassign */
+
 'use server';
 
-type Form = {
-  email: string;
-  username: string;
-  password: string;
-  isTried: boolean;
-  isCorrect: boolean;
-};
+import { z } from 'zod';
 
 const wait = (ms: number) =>
   new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 
-const handleOnSubmit = async (_: Form, current: FormData) => {
+const formScheme = z.object({
+  email: z.string().email().includes('@zod.com'),
+  username: z.string().min(5),
+  password: z
+    .string()
+    .min(10)
+    .regex(/.*[0-9].*/, '숫자를 최소 1개 이상 포함해야 합니다'),
+});
+
+type FormType = z.infer<typeof formScheme>;
+
+type Errors = Partial<Record<keyof FormType, string[]>>;
+
+type FormResult = FormType & {
+  isTried: boolean;
+  errors: Errors;
+};
+
+const handleOnSubmit = async (_: FormResult, current: FormData) => {
   await wait(1000);
 
   const email = current.get('email');
@@ -22,14 +36,31 @@ const handleOnSubmit = async (_: Form, current: FormData) => {
 
   const password = current.get('password');
 
+  const parseResult = formScheme.safeParse({ email, username, password });
+
+  const errorList = parseResult.error?.errors;
+
+  /** parsed error list */
+  const errors = errorList?.reduce<Errors>((a, b) => {
+    const key = b.path[0] as keyof FormType;
+
+    if (key in a) {
+      a[key] = a[key] ? [...a[key], b.message] : [b.message];
+    } else {
+      a[key] = [b.message];
+    }
+
+    return a;
+  }, {});
+
   return {
     email,
     username,
     password,
     isTried: true,
-    isCorrect: !!email && !!username && password === '12345',
+    errors: errors ?? {},
   };
 };
 
 export default handleOnSubmit;
-export type { Form };
+export type { FormResult };
--- app/page.tsx
+++ app/page.tsx
@@ -3,59 +3,63 @@
 'use client';
 
 import { useFormState, useFormStatus } from 'react-dom';
-import handleOnSubmit, { Form } from './action';
+import handleOnSubmit, { FormResult } from './action';
 
-const Submit = ({
-  isCorrect,
-  isTried,
-}: {
-  isCorrect: boolean;
-  isTried: boolean;
-}) => {
+const Submit = ({ isTried, errors }: FormResult) => {
   const { pending } = useFormStatus();
 
   const isNormal = !isTried || pending;
+
+  const isCertified = Object.keys(errors).length === 0;
 
   return (
     <div className="h-lvh w-full flex justify-center">
       <div className="flex flex-col justify-center items-center gap-3 h-full w-80 focus-within::outline-none text-slate-400">
         <input
           type="email"
-          className="ring-2 ring-slate-400 outline-none w-full p-2 rounded-xl"
+          className={`ring-2 ${isNormal ? 'ring-slate-400' : !errors.email ? 'ring-green-400' : 'ring-red-400'} outline-none w-full p-2 rounded-xl`}
           name="email"
           placeholder="Email"
           required
         />
+        <div
+          className={`${isNormal ? 'text-slate-400' : !errors.email ? 'text-green-400' : 'text-red-400'} font-extrabold`}
+        >
+          {isTried && !pending && errors.email?.[0]}
+        </div>
         <input
-          className="ring-2 ring-slate-400 outline-none w-full p-2 rounded-xl"
+          className={`ring-2 ${isNormal ? 'ring-slate-400' : !errors.username ? 'ring-green-400' : 'ring-red-400'} outline-none w-full p-2 rounded-xl`}
           name="username"
           placeholder="Username"
           required
         />
+        <div
+          className={`${isNormal ? 'text-slate-400' : !errors.username ? 'text-green-400' : 'text-red-400'} font-extrabold`}
+        >
+          {isTried && !pending && errors.username?.[0]}
+        </div>
         <input
           type="password"
-          className={`ring-2 ${isNormal ? 'ring-slate-400' : isCorrect ? 'ring-green-400' : 'ring-red-400'} outline-none w-full p-2 rounded-xl`}
+          className={`ring-2 ${isNormal ? 'ring-slate-400' : !errors.password ? 'ring-green-400' : 'ring-red-400'} outline-none w-full p-2 rounded-xl`}
           name="password"
           placeholder="Password"
           required
         />
         <div
-          className={`h-6 ${isNormal ? 'text-slate-400' : isCorrect ? 'text-green-400' : 'text-red-400'} font-extrabold`}
+          className={`${isNormal ? 'text-slate-400' : !errors.password ? 'text-green-400' : 'text-red-400'} font-extrabold`}
         >
-          {isTried &&
-            !pending &&
-            (isCorrect ? 'correct password' : 'wrong password')}
+          {isTried && !pending && errors.password?.[0]}
         </div>
         <button
           type="submit"
           disabled={pending}
-          className={`w-full p-3 rounded-xl font-bold ${isNormal ? 'bg-slate-400' : isCorrect ? 'bg-green-400' : 'bg-red-400'} text-white`}
+          className={`w-full p-3 rounded-xl font-bold ${isNormal ? 'bg-slate-400' : isCertified ? 'bg-green-400' : 'bg-red-400'} text-white`}
         >
           {!isTried
             ? 'submit'
             : pending
               ? 'loading'
-              : isCorrect
+              : isCertified
                 ? "Let's go!"
                 : 'Oh No..'}
         </button>
@@ -65,14 +69,14 @@ const Submit = ({
 };
 
 export default function Home() {
-  const [state, formAction] = useFormState<Form>(
-    handleOnSubmit as unknown as (state: Form) => Form,
+  const [state, formAction] = useFormState<FormResult>(
+    handleOnSubmit as unknown as (state: FormResult) => FormResult,
     {
       email: '',
       username: '',
       password: '',
       isTried: false,
-      isCorrect: false,
+      errors: {},
     },
   );
 
